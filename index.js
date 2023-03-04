a9ce1d0dcf0164591806a473ee7015df0f44affc
zeskord <zeskord@gmail.com>
2023-03-04 15:46:29 +0300

    first

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const path = require('path')
+const fs = require("fs")
+const http = require('http')
+const app = express()
+const multer = require('multer')
+const upload = multer({ dest: 'temp/' })
+// const { spawn } = require('child_process');
+const { exec, spawn } = require('node:child_process');
+
+app.set('json spaces', 2)
+app.use(express.json())
+app.use(express.static("public"))
+app.use(express.json({ limit: '100mb', extended: true, parameterLimit: 5000000 }))
+app.use(express.urlencoded({ limit: '100mb', extended: true, parameterLimit: 5000000 }))
+
+app.post('/test', upload.any(), function (req, res, next) {
+
+    console.log(req.files)
+    jsonfile = req.files[0].filename
+
+    const command = `python3 "main_script.py" "templates/template.docx" ${jsonfile} "temp/result.docx"`
+    console.log(command)
+    exec(command, (err, stdout, stderr) => {
+        if (err) {
+            console.error(err);
+            console.error(stderr);
+            return;
+        }
+        console.log(stdout)
+    })
+
+    // ans = { status: "OK" }
+    // res.json(ans)
+})
+
+app.post('/main', (req, res) => {
+    data = {
+        params: req.body.params,
+        taskId: req.body.taskId
+    }
+    // const buff = Buffer.from(base64, 'base64');
+    jsonFilePath = path.join('./temp/', `${taskId}.json`)
+    fs.writeFileSync(jsonFilePath, params)
+    ans = { status: "OK" }
+    res.json(ans)
+})
+
+
+
+const noSslServer = http.createServer(app)
+noSslServer.listen(8080)
---
+++ main_script.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import sys
+import argparse
+import json
+from docxtpl import DocxTemplate
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT, WD_TAB_ALIGNMENT, WD_LINE_SPACING
+from docx.enum.style import WD_STYLE_TYPE
+from docx.oxml import OxmlElement, ns
+from docx.shared import Cm, Pt
+
+# Инициализация входных параметров.
+def createParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('inputfile', type=str)
+    parser.add_argument('data', type=str)
+    parser.add_argument('otputfile', type=str)
+    return parser
+
+# Основное действие.
+if __name__ == '__main__':
+    
+    # Парсер параметров.
+    parser = createParser()
+    arguments = parser.parse_args(sys.argv[1:])
+
+    print(arguments.inputfile)
+    print(arguments.data)
+    print(arguments.otputfile)
+
+    # Чтение документа-исходника.
+    # doc = docx.Document(arguments.inputfile)
+    doc = DocxTemplate(arguments.inputfile)
+
+    # Чтение входящих параметров.
+    # data = json.load(open(arguments.data, encoding='utf-8'))
+    data = json.load(open(arguments.data, encoding='utf-8-sig'))
+
+    doc.render(data)
+    
+    # Сохраним документ.
+    doc.save(arguments.otputfile)
+
