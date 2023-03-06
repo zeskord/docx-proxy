7fcc3467ffdf9b3f3f183b5d679d1690065f5b28
zeskord <zeskord@gmail.com>
2023-03-07 02:33:08 +0300

    Так

--- questions.py
+++ questions.py
@@ -1,11 +1,11 @@
 
 from docxtpl import DocxTemplate
 
-def questions(doc, data):
+def questions(doc, data, output_file):
     sd = doc.new_subdoc()
     for question_data in data["ВопросыСудьи"]:
         par = sd.add_paragraph(f'Вопрос {question_data["Номер"]}: {question_data["Текст"]}')
-    sd.render(data)
-    return sd
-
-    
+    sd.save(output_file)
+    subdoc = DocxTemplate(output_file)
+    subdoc.render(data)
+    return subdoc
