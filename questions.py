8b3c54a4a7b14cbaddd086941a157c96d5902a20
zeskord <zeskord@gmail.com>
2023-03-07 02:39:55 +0300

    1

--- questions.py
+++ questions.py
@@ -2,10 +2,10 @@
 from docxtpl import DocxTemplate
 
 def questions(doc, data, output_file):
-    sd = doc.new_subdoc()
+    subdoc_template = DocxTemplate()
     for question_data in data["ВопросыСудьи"]:
-        par = sd.add_paragraph(f'Вопрос {question_data["Номер"]}: {question_data["Текст"]}')
-    sd.save(output_file)
+        par = subdoc_template.add_paragraph(f'Вопрос {question_data["Номер"]}: {question_data["Текст"]}')
+    subdoc_template.save(output_file)
     subdoc = DocxTemplate(output_file)
     subdoc.render(data)
     return subdoc
