182c63c4a405ea9617e14d41b7ce30dca5a9fd95
zeskord <zeskord@gmail.com>
2023-03-08 14:56:37 +0300

    .

--- app4_subdoc.py
+++ app4_subdoc.py
@@ -1,15 +1,11 @@
 import base64
 from docxtpl import DocxTemplate, InlineImage
 from docx.shared import Inches, Mm, Pt
-# import jinja2
+from docx.enum.text import WD_ALIGN_PARAGRAPH
 
 def app4_subdoc(data, doc, output_file):
     subdoc_template = DocxTemplate(f'templates/{data["ИмяШаблона_ПриложениеГ"]}')
-    counter = 0
-    # render_data = []
     for file_entry in data["КартинкиПриложенияГ"]:
-        counter = counter + 1
-
         file_guid = file_entry["ИмяФайла"]
         file_extension = file_entry["Расширение"]
         file_base64 = file_entry["ДанныеФайла"]
@@ -19,14 +15,11 @@ def app4_subdoc(data, doc, output_file):
             file.write(image)
 
         sd = subdoc_template.new_subdoc()
-        sd.add_paragraph().add_run().add_picture(filename, width=None, height=Mm(150))
+        par = sd.add_paragraph()
+        par.add_run().add_picture(filename, width=None, height=Mm(200))
+        par.alignment = WD_ALIGN_PARAGRAPH.CENTER
         
-        # file_entry["Картинка"] = InlineImage(subdoc_template, filename, width=Mm(100)),
-        # element_data = {}
         file_entry["Картинка"] = sd
-        # render_data.append(element_data)
-
-    # jinja_env = jinja2.Environment(autoescape=True)
 
     subdoc_template.render(data)
     temp_output_file = f'{output_file}_приложениеГ.docx'
