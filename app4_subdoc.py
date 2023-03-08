012ba829bd9782bca52530e482b6dd423d0a5fa8
zeskord <zeskord@gmail.com>
2023-03-08 14:41:59 +0300

    ,

--- app4_subdoc.py
+++ app4_subdoc.py
@@ -1,11 +1,12 @@
 import base64
 from docxtpl import DocxTemplate, InlineImage
 from docx.shared import Inches, Mm, Pt
-import jinja2
+# import jinja2
 
 def app4_subdoc(data, doc, output_file):
     subdoc_template = DocxTemplate(f'templates/{data["ИмяШаблона_ПриложениеГ"]}')
     counter = 0
+    # render_data = []
     for file_entry in data["КартинкиПриложенияГ"]:
         counter = counter + 1
 
@@ -16,13 +17,18 @@ def app4_subdoc(data, doc, output_file):
         filename = f'temp/{file_guid}.{file_extension}'
         with open(filename, "wb") as file:
             file.write(image)
+
+        sd = subdoc_template.new_subdoc()
+        sd.paragraphs[0].add_run().add_picture(filename, width=Mm(75), height=None)
         
-        file_entry["Картинка"] = InlineImage(subdoc_template, filename, width=Mm(100)),
+        # file_entry["Картинка"] = InlineImage(subdoc_template, filename, width=Mm(100)),
+        # element_data = {}
+        file_entry["Картинка"] = sd
+        # render_data.append(element_data)
 
+    # jinja_env = jinja2.Environment(autoescape=True)
 
-    jinja_env = jinja2.Environment(autoescape=True)
-
-    subdoc_template.render(data, jinja_env)
+    subdoc_template.render(data)
     temp_output_file = f'{output_file}_приложениеГ.docx'
     subdoc_template.save(temp_output_file)
     final = doc.new_subdoc(temp_output_file)
