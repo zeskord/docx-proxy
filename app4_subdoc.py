fc034e65a14ed7a1b9c80d7370384ad04cdf777b
zeskord <zeskord@gmail.com>
2023-03-08 14:53:49 +0300

    .

--- app4_subdoc.py
+++ app4_subdoc.py
@@ -19,7 +19,7 @@ def app4_subdoc(data, doc, output_file):
             file.write(image)
 
         sd = subdoc_template.new_subdoc()
-        sd.add_paragraph().add_run().add_picture(filename, width=Mm(75), height=None)
+        sd.add_paragraph().add_run().add_picture(filename, width=None, height=Mm(150))
         
         # file_entry["Картинка"] = InlineImage(subdoc_template, filename, width=Mm(100)),
         # element_data = {}
